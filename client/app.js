d6356bdc619c3669ffc2192c440d724fcba2969b
WenFeng Zhang <wenfeng0218@icloud.com>
2018-01-17 17:53:37 +0800

    Tab改为4 / Change tab to 4
    Tab改为4 / Change tab to 4

--- client/app.js
+++ client/app.js
@@ -3,11 +3,11 @@ var qcloud = require('./vendor/wafer2-client-sdk/index')
 var config = require('./config')
 var util = require('./utils/util.js')
 
-App({
+App( {
     onLaunch: function () {
-      qcloud.setLoginUrl(config.service.loginUrl);
-      console.log("App:",
-                  util.formatTime(new Date),
-                  "App Launched Success.");
+        qcloud.setLoginUrl(config.service.loginUrl);
+        console.log("App:",
+            util.formatTime(new Date),
+            "App Launched Success.");
     }
-})
+} )
